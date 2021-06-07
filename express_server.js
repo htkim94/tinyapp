4a2b2431b8ce29965c8a2396f08a4c8d09bfdf5b
Hotae Kim <hotae.kim.94@gmail.com>
2021-06-07 19:57:08 +0000

    partials header added to index and show

--- express_server.js
+++ express_server.js
@@ -35,7 +35,12 @@ const urlDatabase = {
 app.get('/urls', (req, res) => {
   const templateVars = { urls: urlDatabase };
   res.render('urls_index', templateVars);
-})
+});
+
+app.get('/urls/:shortURL', (req, res) => {
+  const templateVars = { shortURL: req.params.shortURL, longURL: urlDatabase[req.params.shortURL] };
+  res.render('urls_show', templateVars);
+});
  
 
 app.listen(PORT, () => {
